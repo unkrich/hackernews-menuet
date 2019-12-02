a0525a960e0cf3d03566e04baf94a7a5153f2954
Kevin Unkrich <kev.unkrich@gmail.com>
2019-12-02 13:22:12 -0800

    Added child action menu of post to view user, number of comments, timestamp, link to post, and link to comments

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os/exec"
+	"strings"
 	"time"
 
 	"github.com/caseymrm/menuet"
@@ -35,12 +36,90 @@ func (p *Post) Item(truncate int) menuet.MenuItem {
 		Text:       text,
 		FontWeight: menuet.WeightRegular,
 		Clicked:    p.openLink,
+	}	
+	item.Children = func() []menuet.MenuItem {
+		return p.ChildActions()
 	}
 	return item
 }
 
+func (p *Post) ChildActions() []menuet.MenuItem {
+	// Title wrapped
+	lines := wrap(p.Title, 42)
+	items := make([]menuet.MenuItem, 0, len(lines)+1)
+	for _, line := range lines {
+		items = append(items, menuet.MenuItem{
+			Text:       line,
+			Clicked:    p.openLink,
+			FontWeight:    menuet.WeightMedium,
+		})
+	}
+	// Username
+	items = append(items, menuet.MenuItem{
+		Text:          fmt.Sprintf("By: %s", p.Username),
+		Clicked:       p.openUserProfile,
+		FontWeight:    menuet.WeightUltraLight,
+	})
+	// Timestamp posted
+	items = append(items, menuet.MenuItem{
+		Text: fmt.Sprintf("%s", p.TimestampString),
+		FontWeight: menuet.WeightUltraLight,
+	})
+
+	items = append(items, menuet.MenuItem{
+		Type: menuet.Separator,
+	})
+
+	//Comments
+	items = append(items, menuet.MenuItem{
+		Text:      fmt.Sprintf("Comments: %d", p.CommentCount),
+		Clicked:   p.openComments,
+	})
+	// Upvote - disabled action because not signed in, no authHash
+	if p.CanUpvote {
+		items = append(items, menuet.MenuItem{
+			Text:      fmt.Sprintf("Upvotes: %d (Click to Upvote)", p.Points),
+			Clicked:   p.upvote,
+		})
+	} else {
+		items = append(items, menuet.MenuItem{
+			Text:      fmt.Sprintf("Upvotes: %d", p.Points),
+		})
+	}
+
+	return items
+}
+
+// Post Actions
+func (p *Post) upvote() {
+	exec.Command("curl", p.voteHref()).Run()
+	p.CanUpvote = false
+	menuet.App().MenuChanged()
+}
+
 func (p *Post) openLink() {
 	exec.Command("open", p.Link).Run()
+}
+
+func (p *Post) openUserProfile() {
+	exec.Command("open", p.userHref()).Run()
+}
+
+func (p *Post) openComments() {
+	exec.Command("open", p.commentsHref()).Run()
+}
+
+// Post links formatted
+func (p *Post) commentsHref() string {
+	return fmt.Sprintf("https://news.ycombinator.com/item?id=%d", p.ID)
+}
+
+func (p *Post) voteHref() string {
+	return fmt.Sprintf("https://news.ycombinator.com/vote?id=%d&auth=%s", p.ID, p.AuthHash)
+}
+
+func (p *Post) userHref() string {
+	return fmt.Sprintf("https://news.ycombinator.com/user?id=%s", p.Username)
 }
 
 // User represents one HackerNews user
@@ -71,7 +150,7 @@ func checkHackerNews() {
 			log.Printf("Error: %v", err)
 			continue
 		}
-		
+
 		menuet.App().MenuChanged()
 	}
 }
@@ -88,6 +167,30 @@ func menuItems() []menuet.MenuItem {
 		items = append(items, post.Item(50))
 	}
 	return items
+}
+
+
+// Helper
+func wrap(text string, width int) []string {
+	lines := make([]string, 0, len(text)/width)
+	words := strings.Fields(text)
+	if len(words) == 0 {
+		return lines
+	}
+	current := words[0]
+	remaining := width - len(current)
+	for _, word := range words[1:] {
+		if len(word)+1 > remaining {
+			lines = append(lines, current)
+			current = word
+			remaining = width - len(word)
+		} else {
+			current += " " + word
+			remaining -= 1 + len(word)
+		}
+	}
+	lines = append(lines, current)
+	return lines
 }
 
 
